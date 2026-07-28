1b3e02dbcd7b118d5472fe3df4e82695aefba73a
Abhinav Ashwin Apte <182189877+abesin8@users.noreply.github.com>
2026-07-28 18:55:59 +0530

    Time: 11 ms (37.20%) | Memory: 16.1 MB (5.28%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,14 +4,16 @@ public:
         int n=nums.size();
         int k=target;
         vector<int> v;
+        unordered_map<int,int> m;
         for(int i=0;i<n;i++){
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]==k-nums[i]){
-                        v.push_back(i);
-                        v.push_back(j);
-                        return v;
-                    }
-                
+            m[nums[i]]=i;
+        }
+        for(int i=0;i<n;i++){
+            int diff=k-nums[i];
+            if(m.find(diff)!=m.end() && m[diff]!=i){
+                v.push_back(i);
+                v.push_back(m[diff]);
+                return v;
             }
         }
         return v;
