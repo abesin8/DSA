434f7043f0c6716fb6da6ec978ba4ca299f347f4
Abhinav Ashwin Apte <182189877+abesin8@users.noreply.github.com>
2026-07-28 19:18:55 +0530

    Time: 2 ms (72.17%) | Memory: 14.9 MB (21.45%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,20 +2,42 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        int k=target;
         vector<int> v;
-        unordered_map<int,int> m;
+        vector<pair<int,int>> ind;
         for(int i=0;i<n;i++){
-            m[nums[i]]=i;
+            ind.push_back({nums[i],i});
         }
-        for(int i=0;i<n;i++){
-            int diff=k-nums[i];
-            if(m.find(diff)!=m.end() && m[diff]!=i){
-                v.push_back(i);
-                v.push_back(m[diff]);
+        sort(ind.begin(),ind.end());
+
+        int i=0,j=n-1;
+        while(i<j){
+            int sum = ind[i].first + ind[j].first;
+            if(sum==target){
+                v.push_back(ind[i].second);
+                v.push_back(ind[j].second);
                 return v;
+            }
+            if(sum<target){
+                i++;
+            }
+            else{
+                j--;
             }
         }
         return v;
+        
+        // unordered_map<int,int> m;
+        // for(int i=0;i<n;i++){
+        //     m[nums[i]]=i;
+        // }
+        // for(int i=0;i<n;i++){
+        //     int diff=k-nums[i];
+        //     if(m.find(diff)!=m.end() && m[diff]!=i){
+        //         v.push_back(i);
+        //         v.push_back(m[diff]);
+        //         return v;
+        //     }
+        // }
+        // return v;
     }
 };
