13d8881a9020346cf476146dc99a5a695a6c0b49
Abhinav Ashwin Apte <182189877+abesin8@users.noreply.github.com>
2026-08-03 20:41:49 +0530

    Time: 15 ms (92.42%) | Memory: 17.4 MB (56.24%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n=nums.size();
-        set<vector<int>> st;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
         for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
-                set<long long> hash;
-                for(int k=j+1;k<n;k++){
-                    long long sum = (long long)nums[i]+nums[j]+nums[k];
-                    long long rem= target -sum;
-                    if(hash.find(rem)!=hash.end()){
-                        vector<int> temp={nums[i],nums[j],nums[k],(int)rem};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum= (long long )nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum>target){
+                        l--;
                     }
-                    hash.insert(nums[k]);
+                    else if(sum<target){
+                        k++;
+                    }
+                    else{
+                        vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++; 
+                        while(k<l && nums[l]==nums[l+1]) l--; 
+                    }
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(),st.end());
         return ans;
-
     }
 };
